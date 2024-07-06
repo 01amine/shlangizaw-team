9cc3120edc02f4e1db8f0b74ae4aaa17950a2731
its0amine <mohamedamine.heddouche@etu.usthb.dz>
2024-07-06 16:13:50 +0100

    final

--- challenge-7/index.js
+++ challenge-7/index.js
@@ -1,73 +1,106 @@
 const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
-const path = require('path');
-const mongoose = require('mongoose');
 const connectDB = require('./config/database');
 const authRoutes = require('./routes/auth');
 const meetingRoutes = require('./routes/meeting');
 const chatRoutes = require('./routes/chat');
 const cookieParser = require('cookie-parser');
 const jwt = require('jsonwebtoken');
+const Message = require('./models/Message');
+const User = require('./models/User');
+const authmiddlware = require('./middlewares/auth');
+
+const app = express();
+
+const server = http.createServer(app);
+const io = socketIo(server);
+
 require('dotenv').config();
 
 connectDB();
 
 const PORT = 3000
 
-const app = express();
-const server = http.createServer(app);
-const io = socketIo(server);
+
+
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
+
 
 app.set('view engine', 'ejs');
 app.set('views','views');
 
 app.use(express.static('public'));
 
-app.use((req, res, next) => {
-  const token = req.cookies.token;
-  if (token) {
-    try {
-      const decoded = jwt.verify(token, process.env.JWT_SECRET);
-      req.user = decoded;
-    } catch (err) {
-      console.error(err);
-    }
-  }
-  next();
-});
+
+
 
 app.use('/auth', authRoutes);
 app.use('/meeting', meetingRoutes);
 app.use('/chat', chatRoutes);
-
 app.get('/', (req, res) => {
   res.render('index');
 });
+app.get('/home', authmiddlware,(req,res)=>{
+  res.render('home');
+})
 app.get('/auth/register', (req, res) => {
   res.render('auth/register');
 });
 app.get('/auth/login', (req, res) => {
   res.render('auth/login');
 });
-app.get('/meeting/create', (req, res) => {
+app.get('/meeting/create',authmiddlware,(req, res) => {
   res.render('meeting/create');
 });
-app.get('/meeting/join', (req, res) => {
+app.get('/meeting/join',authmiddlware, (req, res) => {
   res.render('meeting/join');
 });
 
-io.on('connection', (socket) => {
-  socket.on('join-meeting', (roomId, userId) => {
-    socket.join(roomId);
-    socket.broadcast.to(roomId).emit('user-connected', userId);
-    socket.on('send-chat-message', (roomId, message) => {
-      io.to(roomId).emit('chat-message', message);
+
+
+io.use((socket, next) => {
+  const token = socket.handshake.auth.token;
+  if (token) {
+    jwt.verify(token, 'your_jwt_secret', (err, decoded) => {
+      if (err) {
+        return next(new Error('Authentication error'));
+      }
+      socket.userId = decoded.id;
+      next();
     });
+  } else {
+    next(new Error('Authentication error'));
+  }
+});
+
+io.on('connection', socket => {
+  socket.on('join-meeting', async (roomId) => {
+    const user = await User.findById(socket.userId);
+    if (!user) {
+      return socket.disconnect();
+    }
+
+    socket.join(roomId);
+    socket.to(roomId).emit('user-connected', socket.userId);
+  });
+
+  socket.on('send-chat-message', async (roomId, message) => {
+    const messageObj = new Message({
+      roomId,
+      message,
+      userId: socket.userId
+    });
+
+    await messageObj.save();
+    io.to(roomId).emit('chat-message', message);
+  });
+
+  socket.on('share-screen', (roomId, screenStreamId) => {
+    io.to(roomId).emit('screen-shared', screenStreamId);
   });
 });
 
